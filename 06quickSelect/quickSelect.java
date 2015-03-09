fdda53e3d0b76441b4070d0cdd2e59818727ad92
andy5313 <andyz5313@yahoo.com>
2015-03-09 19:10:14 -0400

    quick select

--- 06quickSelect/quickSelect.java
+++ 06quickSelect/quickSelect.java
@@ -1,38 +1,48 @@
-import java.util.*;
+import java.util.Arrays;
+
 public class quickSelect{
 
-    public static void partition( int[] ary, int si, int ei ){
-	Random rand = new Random();
-	int pivot = rand.nextInt(ei+1-si) + si;
-	int[] D = new int[ary.length];
-	int first = si;
-	int last = ei;
-	
-	for (int i = si; i <= ei; i ++){
-	    if (ary[i] < ary[pivot]){
-		D[first] = ary[i];
-		first ++;
-	    }
-	    if (ary[i] > ary[pivot]){
-		D[last] = ary[i];
-		last --;
+    public static int quickselect(int[] ary,int index){
+	int si = 0;
+	int ei = ary.length-1;
+	int partindex = -1;
+
+	while (index != partindex){
+	    partindex = partition(ary,si,ei);
+	    if (index > partindex){
+		si = partindex;
+		partindex = partition(ary,si,ei);
+	    }else if (index < partindex){
+		ei = partindex;
+		partindex = partition(ary,si,ei);
 	    }
 	}
-	D[first] = ary[pivot];
-	
-	for (int i = si - 1; i >= 0; i --){
-	    D[i] = ary[i];
-	}
-	for (int i = ei + 1; i < ary.length; i ++){
-	    D[i] = ary[i];
-	}
+	return ary[partindex];
     }
 
-    public static void main(String[] args){
-	int [] a = {5, 7, 10, 8, 1, 6 , 3 , 4, 9, 2};
-	partition(a, 0, 9);
-	for (int i = 0; i < a.length; i ++){
-	    System.out.print(a[i] + " ");
+    public static int partition(int[] ary,int si, int ei){
+	int ri = si + (int)(Math.random()*(ei-si+1));
+	int pivot = ary[ri];
+	int end = ei;
+	int[] copy= new int[ary.length];
+	for (int i=0;i<ary.length;i++){
+	    if (i<si || i>ei){
+		copy[i] = ary[i];
+	    }
 	}
+	for (int i=si;i<=end;i++){
+	    if (ary[i] < pivot){
+		copy[si] = ary[i];
+		si++;
+	    }else if (ary[i] > pivot){
+		copy[ei] = ary[i];
+		ei--;
+	    }
+	}
+	copy[si] = pivot;
+	System.arraycopy(copy,0,ary,0,ary.length);
+	return si;
     }
+
+
 }
