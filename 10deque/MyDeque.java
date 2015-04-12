0b6650605aaebb36dbf69b309c69c2d85b65f908
andy5313 <andyz5313@yahoo.com>
2015-04-12 08:26:33 -0400

    deque

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -4,6 +4,9 @@ public class MyDeque<T>{
     private int head,tail,size;
     private Object[] array;
 
+    public String name(){
+	return "andy.zheng";
+    }
     public MyDeque(){
 	array = new Object[10];
 	head = 4;
@@ -12,43 +15,99 @@ public class MyDeque<T>{
     }
 
     public void addFirst(T value){
-	array[head] = value;
+	if (size == array.length){
+	    expand();
+	}
 	head--;
-	size++;
 	if (head == -1){
 	    head = array.length-1;
 	}
+	array[head] = value;
+	size++;
+
     }
 
     public void addLast(T value){
-	array[tail] = value;
+	if (size == array.length){
+	    expand();
+	}
 	tail++;
-	size++;
 	if(tail == array.length){
 	    tail = 0;
 	}
+	array[tail] = value;
+	size++;
+
     }
 
     public T removeFirst(){
-	if (head == array.length -1){
-	    head = 0;
-	}else{
-	    head++;
+	if (size == 0){
+	    throw new NoSuchElementException();
 	}
-	T temp =(T)array[head];
-	array[head] = null;
-	return temp;	
+	size --;
+	T first = (T)array[head];
+	head ++;
+	if (head == array.length){
+	    head = 0;
+	}
+	return first;	
     }
 
     public T removeLast(){
-	if (tail == 0){
-	    tail = array.length-1;
-	}else{
-	    tail--;
+	if (size == 0){
+	    throw new NoSuchElementException();
 	}
-	T save = (T)array[tail];
-	array[tail] = null;
-	return save;
+	size --;
+	T last = (T)array[tail];
+	tail --;
+	if (tail == -1){
+	    tail = array.length - 1;
+	}
+	return last;
+    }
+    
+    public void expand(){
+	Object[] a = new Object[array.length * 2];
+	if (head < tail){
+	    for (int i = head; i <= tail; i ++){
+		a[i] = array[i];
+	    }
+	}else{
+	    for (int i = head; i < array.length; i ++){
+		a[i] = array[i];
+	    }
+	    for (int i = 0; i <= tail; i ++){
+		a[i + array.length] = array[i];
+	    }
+	    tail += array.length;
+	}
+        array = a;
+    }    
+
+    public String toString(){
+	String s = "[ ";
+	if (size > 0){
+	    if (head <= tail){
+		for (int i = head; i <= tail; i ++){
+		    s += array[i] + " ";
+		}
+	    }else{
+		for (int i = head; i < array.length; i ++){
+		    s += array[i] + " ";
+		}
+		for (int i = 0; i <= tail; i ++){
+		    s += array[i] + " ";
+		}
+	    }
+	}
+	return s + "]";
+    }
+
+    public static void main(String[] args){
+	
+	MyDeque<String> d = new MyDeque<String>();
+	System.out.println(d);
+
     }
     
 
